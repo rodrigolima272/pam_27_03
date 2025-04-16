e620f8b58f195f0106faa82b22f9676d5c93165a
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-04-16 17:56:25 -0300

    exercicio Form

---
+++ app/form.tsx
@@ -0,0 +1,15 @@
+import CustomButton from "@/components/Title";
+import React, { useState } from "react";
+import { Button, Text, TextInput, View, Form, Input, Label} from "react-native";
+
+<Veiw>
+  <Form>
+    <Label>Digite o seu nome</Label>
+    <Input type="text"/>
+    <Label>Digite o seu sobrenome</Label>
+    <Input type="text"/>
+    <Label>Digite algo</Label>
+    <Input type="text"/>
+    <Input type="submit"/>
+  </Form>
+</Veiw>
