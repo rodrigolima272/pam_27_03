2c98a3703d1ea5458d26a3f1a80de18bdec1e045
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-04-24 15:55:26 -0300

    Edit Form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-
-import {TextInput, Veiw} from "react-native";
+import React {useState} from "react";
+import {Text, TextInput, Veiw} from "react-native";
 import {CustomButton} from './Title'
 
 export default function Form(){
+const [nome, setNome] = useState(' ');
+const [sobrenome, setSobrenome] = useState(' ');
+const [instagram, setInstagram] = useState(' ');
+  
   return(
     <Veiw>
       <TextInput placeholder="Nome" onChangeText = []></TextInput>
       <TextInput placeholder="Sobrenome" onChangeText = []></TextInput>
       <TextInput placeholder="Instagram" onChangeText = []></TextInput>
+
+      <Text>{}, {}, {}<Text/>
 
       <CustomButton title="Cadastrar" onPress {{} => {}}/>
     </Veiw>
